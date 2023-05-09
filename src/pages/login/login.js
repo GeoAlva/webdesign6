712e7d7958037dcc8dd2e9db4697e204a133dd79
GeoAlva <alvarado.geovanny.98@gmail.com>
2023-05-09 16:36:14 +0200

    login page base

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,48 @@
-
+import "./login.css"
 
 export default function Login() {
     return (
-        <div className='left'>
-            <h1>Login</h1><h1>Login</h1><h1>Login</h1><h1>Login</h1><h1>Login</h1><h1>Login</h1>
-        </div>
+
+        <div class="login">
+            <div class="left-slogan">
+
+                <h1 class="slogan">Ciao</h1>
+                <br />
+                <p class="text_fg">Inizia subito a costruire il tuo curriculum ideale e <br />
+                    preparati a conseguire il lavoro che hai sempre <br /> sognato</p>
+
+
+            </div>
+            <div class="right-form">
+
+
+                <form action="#" method="post" class="form">
+                    <h2 class="benvenuto">Login</h2>
+
+
+                    <div class="form-element">
+                        <img src="images/Persona.png" alt="" class="icons" />
+                        <input type="email" id="email" name="email" class="form-input" placeholder=" " required />
+                        <label class="floating-label" for="email">Username / e-mail</label>
+                    </div>
+
+                    <div class="form-element">
+                        <img src="images/Lucchetto.png" alt="" class="icons" />
+                        <input type="password" id="pass" name="pass" class="form-input" placeholder=" " required />
+                        <label class="floating-label" for="pass">Password</label>
+                    </div>
+                    <a href="#" class="forgot">Password dimenticata?</a>
+
+                    <input type="submit" id="submit" name="submit" value="Accedi" class="btn" /> <br /><br />
+                    <p class="account"> Non hai un account?</p>
+                    <a href="index.html">Registrati</a>
+                    <br />
+                </form>
+
+
+            </div>
+        </div >
+
+
     )
 }
