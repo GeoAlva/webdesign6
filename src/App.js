8ff73154f523c107449671839f0d965855f8db24
GeoAlva <alvarado.geovanny.98@gmail.com>
2023-05-18 17:50:07 +0200

    Merged carusel and searchbar into home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import AppHeader from './components/header/header';
-import Home from './pages/home';
+import Home from './pages/home/home';
 import Contacts from './pages/contacts'
 import Login from './pages/login/login';
 import Signup from './pages/signup/signup';
