6e7d9f4a8aef7ba7492e02153a0ff37ae6607e6a
DaniGagge <92739511+DaniGagge@users.noreply.github.com>
2023-06-21 19:02:16 +0200

    Update login.js
    Aggiunta la funziona per controllare se mail e pass inserite sono presenti nella tabella. Se sono presenti redireziono (poi dobbiamo vedere cosa fare)

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,51 +1,90 @@
-import "./login.css"
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Airtable from 'airtable';
 
 export default function Login() {
-    return (
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
 
-        <div class="login">
-            <div class="left-slogan">
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.pass.value;
 
-                <h1 class="slogan">Ciao</h1>
-                <br />
-                <p class="text_fg">Inizia subito a costruire il tuo curriculum<br />
-                    e trova il lavoro che hai sempre sognato.</p>
+    try {
+      const userExists = await checkUserExists(email, password);
 
+      if (userExists) {
+        navigate('/home'); //ho messo una pagina a caso per testare
+      } else {
+        setError('Account non trovato!');
+      }
+    } catch (error) {
+      console.error('Form action error', error);
+    }
+  };
 
-            </div>
-            <div class="right-form">
+  return (
+    <div className="login">
+      <div className="left-slogan">
+        <h1 className="slogan">Ciao</h1>
+        <br />
+        <p className="text_fg">Inizia subito a costruire il tuo curriculum<br />e trova il lavoro che hai sempre sognato.</p>
+      </div>
+      <div className="right-form">
+        <form onSubmit={handleSubmit} method="post" className="form">
+          <h2 className="benvenuto">Login</h2>
 
+          <div className="form-element">
+            <img src="images/Persona.png" alt="" className="icons" />
+            <input type="email" id="email" name="email" className="form-input" placeholder=" " required />
+            <label className="floating-label" htmlFor="email">
+              E-mail
+            </label>
+          </div>
 
-                <form action="#" method="post" class="form">
-                    <h2 class="benvenuto">Login</h2>
+          <div className="form-element">
+            <img src="images/Lucchetto.png" alt="" className="icons" />
+            <input type="password" id="pass" name="pass" className="form-input" placeholder=" " required />
+            <label className="floating-label" htmlFor="pass">
+              Password
+            </label>
+          </div>
+          <a href="#" className="forgot">
+            Password dimenticata?
+          </a>
 
+          {error && <p className="error">{error}</p>}
 
-                    <div class="form-element">
-                        <img src="images/Persona.png" alt="" class="icons" />
-                        <input type="email" id="email" name="email" class="form-input" placeholder=" " required />
-                        <label class="floating-label" for="email">E-mail</label>
-                    </div>
+          <input type="submit" id="submit" name="submit" value="Accedi" className="btn" />
+          <p className="account">Non hai un account?</p>
+          <a href="/signup">Registrati</a>
+          <br />
+        </form>
 
-                    <div class="form-element">
-                        <img src="images/Lucchetto.png" alt="" class="icons" />
-                        <input type="password" id="pass" name="pass" class="form-input" placeholder=" " required />
-                        <label class="floating-label" for="pass">Password</label>
-                    </div>
-                    <a href="#" class="forgot">Password dimenticata?</a>
+        <div className="bg"></div>
+      </div>
+    </div>
+  );
+}
 
-                    <input type="submit" id="submit" name="submit" value="Accedi" class="btn" />
-                    <p class="account"> Non hai un account?</p>
-                    <a href="/signup">Registrati</a>
-                    <br />
-                </form>
+async function checkUserExists(email, password) {
+  const base = new Airtable({ apiKey: 'keyIXV1obmywgbWZE' }).base('app7EHcO1NO4VD6sc');
+  const table = base('Utenti');
 
-                <div class="bg">
-                </div>
+  return new Promise((resolve, reject) => {
+    table
+      .select({
+        filterByFormula: `AND(Email = '${email}', Password = '${password}')`,
+        maxRecords: 1,
+      })
+      .firstPage((err, records) => {
+        if (err) {
+          reject(err);
+          return;
+        }
 
-
-            </div>
-        </div >
-
-
-    )
+        resolve(records.length > 0);
+      });
+  });
 }
