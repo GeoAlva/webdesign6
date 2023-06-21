1a9a899885795b2be0a92a83b38bc35632a7af3a
DaniGagge <92739511+DaniGagge@users.noreply.github.com>
2023-06-21 19:00:35 +0200

    Aggiungi controlli
    - Controllo che la mail inserita non esista già nella tabella

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,126 +1,109 @@
-import "../login/login.css"
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Airtable from 'airtable';
-import React,{ useEffect,useState } from 'react';
-import { useNavigate } from "react-router-dom";
-
 
 export default function Signin() {
   const navigate = useNavigate();
-  const handleSubmit = async (event) => {
+  const [error, setError] = useState('');
 
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const email = event.target.elements.email.value;
     const password = event.target.elements.pass.value;
-    // Initialize the Airtable base
-    const base = new Airtable({ apiKey: 'keyIXV1obmywgbWZE' }).base('app7EHcO1NO4VD6sc');
-  
+    const confirmPassword = event.target.elements.confirm_pass.value;
+
+    if (password !== confirmPassword) {
+      setError('Errore: le due password inserite non sono uguali!');
+      return;
+    }
+
     try {
-      // Perform the Airtable form action
+      const userExists = await checkEmailExists(email);
+
+      if (userExists) {
+        setError('Errore: l\'indirizzo email è già registrato!');
+        return;
+      }
+
+      const base = new Airtable({ apiKey: 'keyIXV1obmywgbWZE' }).base('app7EHcO1NO4VD6sc');
       const response = await base('Utenti').create({
-        // Set the fields you want to send to Airtable
         Email: email,
-        Password: password
+        Password: password,
       });
       console.log('Form action successful', response);
-      navigate("/login");
+      navigate('/login');
     } catch (error) {
       console.error('Form action error', error);
     }
   };
-  
-    return (
-        <div class="login" >
-            <div class="left-slogan">
 
-                <h1 class="slogan">Ciao</h1>
-                <br />
-                <p class="text_fg">Inizia subito a costruire il tuo curriculum ideale e <br />
-                    preparati a conseguire il lavoro che hai sempre <br /> sognato</p>
-            </div>
-            <div class="right-form">
+  return (
+    <div className="login">
+      <div className="left-slogan">
+        <h1 className="slogan">Ciao</h1>
+        <br />
+        <p className="text_fg">
+          Inizia subito a costruire il tuo curriculum ideale e <br />
+          preparati a conseguire il lavoro che hai sempre <br /> sognato
+        </p>
+      </div>
+      <div className="right-form">
+        <form onSubmit={handleSubmit} method="post" className="form">
+          <h2 className="benvenuto">Registrati</h2>
 
-
-                <form onSubmit={handleSubmit} method="post" class="form">
-                    <h2 class="benvenuto">Registrati</h2>
-
-          <div class="form-element">
-            <img src="images/Persona.png" alt="" class="icons" />
-            <input type="email" id="email" name="email" class="form-input" placeholder=" " required />
-            <label class="floating-label" for="email">E-mail</label>
+          <div className="form-element">
+            <img src="images/Persona.png" alt="" className="icons" />
+            <input type="email" id="email" name="email" className="form-input" placeholder=" " required />
+            <label className="floating-label" htmlFor="email">
+              E-mail
+            </label>
           </div>
 
-          <div class="form-element">
-            <img src="images/Lucchetto.png" alt="" class="icons" />
-            <input type="password" id="pass" name="pass" class="form-input" placeholder=" " required />
-            <label class="floating-label" for="pass">Password</label>
+          <div className="form-element">
+            <img src="images/Lucchetto.png" alt="" className="icons" />
+            <input type="password" id="pass" name="pass" className="form-input" placeholder=" " required />
+            <label className="floating-label" htmlFor="pass">
+              Password
+            </label>
           </div>
 
-          <div class="form-element">
-            <img src="images/Lucchetto.png" alt="" class="icons" />
-            <input type="password" id="confirm_pass" name="confirm_pass" class="form-input" placeholder=" " required />
-            <label class="floating-label" for="confirm_pass">Conferma password</label>
+          <div className="form-element">
+            <img src="images/Lucchetto.png" alt="" className="icons" />
+            <input
+              type="password"
+              id="confirm_pass"
+              name="confirm_pass"
+              className="form-input"
+              placeholder=" "
+              required
+            />
+            <label className="floating-label" htmlFor="confirm_pass">
+              Conferma password
+            </label>
           </div>
 
-          <input type="submit" id="submit" name="submit" value="Registrati" class="btn" />
-          <p class="account"> Hai già un account?</p>
+          {error && <p className="error">{error}</p>}
+
+          <input type="submit" id="submit" name="submit" value="Registrati" className="btn" />
+          <p className="account">Hai già un account?</p>
           <a href="/login">Accedi</a>
           <br />
         </form>
 
-        <div class="bg">
-
-        </div>
+        <div className="bg"></div>
       </div>
-    </div >
-
-
-  )
-}
-
-function UserExistsComponent() {
-  const [userExists, setUserExists] = useState(false);
-  const [userInfo, setUserInfo] = useState(null);
-
-  const email = 'marco@jh'; // rimpiazzare con l'elemento del record da cercare
-
-  useEffect(() => {
-    checkUserExists(email)
-      .then((result) => {
-        setUserExists(result.exists);
-        if (result.exists) {
-          setUserInfo(result.userInfo);
-        }
-      })
-      .catch((error) => console.error('Error checking user existence:', error));
-  }, [email]);
-
-  return (
-    <div>
-      {userExists ? (
-        <div>
-          <p>User {email} exists</p>
-          {userInfo && (
-            <div>
-              <p>Password: {userInfo.password}</p>
-              {/* Add more fields here if needed */}
-            </div>
-          )}
-        </div>
-      ) : (
-        <p>User does not exist</p>
-      )}
     </div>
   );
 }
 
-function checkUserExists(email) {
-    const base = new Airtable({ apiKey: 'keyIXV1obmywgbWZE' }).base('app7EHcO1NO4VD6sc'); //api key e link alla pagina
-    const table = base('Utenti'); // nome della tabella da cui si prendono le informazioni
+async function checkEmailExists(email) {
+  const base = new Airtable({ apiKey: 'keyIXV1obmywgbWZE' }).base('app7EHcO1NO4VD6sc');
+  const table = base('Utenti');
 
   return new Promise((resolve, reject) => {
     table
       .select({
-        filterByFormula: `Email = '${email}'`, // nome del campo da cui prendere le informazioni
+        filterByFormula: `Email = '${email}'`,
         maxRecords: 1,
       })
       .firstPage((err, records) => {
@@ -129,19 +112,7 @@ function checkUserExists(email) {
           return;
         }
 
-        if (records.length > 0) {
-          // User exists
-          const user = records[0];
-          const userInfo = {
-            password: user.fields.Password, // rimpiazzare col nome del campo dal quale si vogliono ottenere i dati
-            // Add more fields here if needed
-          };
-
-          resolve({ exists: true, userInfo });
-        } else {
-          // User does not exist
-          resolve({ exists: false, userInfo: null });
-        }
+        resolve(records.length > 0);
       });
   });
 }
